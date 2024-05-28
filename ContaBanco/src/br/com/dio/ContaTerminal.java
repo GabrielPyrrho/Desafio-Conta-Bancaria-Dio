877c52dd7a9c4d4f97ca1d416ebb22ddae31e065
GabrielPyrrho <pyrrhogabriel17@gmail.com>
2024-05-28 17:21:48 -0300

    Desafio conta bancaria

---
+++ ContaBanco/src/br/com/dio/ContaTerminal.java
@@ -0,0 +1,42 @@
+package br.com.dio;
+
+import java.math.BigDecimal;
+
+public class ContaTerminal {
+    private String agencia;
+    private Integer numero;
+    private String nomeCliente;
+    private Double saldo;
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+}
